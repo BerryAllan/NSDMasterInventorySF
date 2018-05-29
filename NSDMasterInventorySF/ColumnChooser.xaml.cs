f3d7224c6dcba6afb30f3cdb4dbad4279b12feb9
BerryAllan <titomathias73@gmail.com>
2018-05-29 14:13:31 -0500

    Hopefully Good to Go 4
    Signed-off-by: BerryAllan <titomathias73@gmail.com>

---
+++ NSDMasterInventorySF/ColumnChooser.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Syncfusion.Linq;
+
+namespace NSDMasterInventorySF
+{
+	/// <summary>
+	/// Interaction logic for ColumnChooser.xaml
+	/// </summary>
+	public partial class ColumnChooser
+	{
+		public static RoutedCommand CloseWindow = new RoutedCommand();
+		private string _chosenColumn;
+		private ComboBoxBuilder _builder;
+
+		public ColumnChooser(DataTable prefabTable, DataTable comboTable, ComboBoxBuilder builder)
+		{
+			_builder = builder;
+			CloseWindow.InputGestures.Add(new KeyGesture(Key.Escape));
+			CommandBindings.Add(new CommandBinding(CloseWindow, CloseCurrentWindow));
+
+			InitializeComponent();
+
+			List<string> columnNames = new List<string>();
+			foreach(DataColumn col in comboTable.Columns)
+				columnNames.Add(col.ColumnName);
+
+			foreach (DataRow row in prefabTable.Rows)
+				if ((row[1].ToString().ToLower().Equals("autocomplete") || row[1].ToString().ToLower().Equals("combobox")) &&
+				    !comboTable.Columns.ToList<DataColumn>().Select(column => column.ColumnName).Contains(row[0]))
+					ViableColumnList.Items.Add(row[0].ToString());
+		}
+
+		private void DoneButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			_chosenColumn = ViableColumnList.SelectedItem.ToString();
+			if (!string.IsNullOrEmpty(_chosenColumn))
+				_builder.ComboTable.Columns.Add(_chosenColumn);
+			_builder.ComboGrid.ItemsSource = _builder.ComboTable;
+			_builder.GenerateColumns();
+			Close();
+		}
+
+		private void CloseCurrentWindow(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void ViableColumnList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			DoneButton.IsEnabled = ViableColumnList.SelectedItems.Count > 0;
+		}
+	}
+}
---
+++ NSDMasterInventorySF/GenerateFromTable.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using NSDMasterInventorySF.Properties;
+
+namespace NSDMasterInventorySF
+{
+	/// <summary>
+	/// Interaction logic for GenerateFromTable.xaml
+	/// </summary>
+	public partial class GenerateFromTable
+	{
+		private readonly PrefabManager _mgr;
+
+		public GenerateFromTable(PrefabManager mgr)
+		{
+			InitializeComponent();
+			_mgr = mgr;
+
+			foreach (DataTable table in MainWindow.MasterDataSet.Tables)
+				TableList.Items.Add(table.TableName);
+		}
+
+		private void DoneButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			using (var conn = new SqlConnection(App.ConnectionString))
+			{
+				conn.Open();
+				if (App.GetTableNames(conn, $"{Settings.Default.Schema}_PREFABS").Contains(PrefabNameBox.Text))
+					MessageBox.Show("Identical Prefab exists. Please rename.", "Warning", MessageBoxButton.OK,
+						MessageBoxImage.Warning);
+
+				DataTable prefabTable = new DataTable();
+				prefabTable.Columns.Add("COLUMNS");
+				prefabTable.Columns.Add("TYPES");
+				prefabTable.Columns.Add("SORTBYS");
+				prefabTable.Columns.Add("GROUPS");
+				foreach (DataColumn column in MainWindow.MasterDataSet.Tables[TableList.SelectedItem.ToString()].Columns)
+				{
+					DataRow dr = prefabTable.NewRow();
+					dr[0] = column.ColumnName;
+					dr[1] = "TextField";
+					prefabTable.Rows.Add(dr);
+				}
+
+				if (conn.State != ConnectionState.Open)
+					conn.Open();
+				using (var comm =
+					new SqlCommand(
+						$"CREATE TABLE [{Settings.Default.Schema}_PREFABS].[{PrefabNameBox.Text}] (COLUMNS TEXT, TYPES TEXT, SORTBYS TEXT, GROUPS TEXT)",
+						conn))
+				{
+					comm.ExecuteNonQuery();
+				}
+
+				var bulkCopy =
+					new SqlBulkCopy(conn)
+					{
+						DestinationTableName =
+							$"[{Settings.Default.Schema}_PREFABS].[{PrefabNameBox.Text}]"
+					};
+				bulkCopy.WriteToServer(prefabTable);
+				conn.Close();
+			}
+			_mgr.PopulateListBox();
+			Close();
+		}
+
+		private void TableList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			DoneButton.IsEnabled = TableList.SelectedItems.Count > 0 && !string.IsNullOrEmpty(PrefabNameBox.Text);
+		}
+
+		private void PrefabNameBox_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			DoneButton.IsEnabled = TableList.SelectedItems.Count > 0 && !string.IsNullOrEmpty(PrefabNameBox.Text);
+		}
+	}
+}
